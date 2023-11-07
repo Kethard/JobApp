4bc0c37416821aaeb546ead2b899efcf26bef84b
Konrad <konmajch@gmail.com>
2023-11-07 22:10:52 +0100

    relations one to one

--- app/admin.py
+++ app/admin.py
@@ -5,5 +5,14 @@ class JobAdmin(admin.ModelAdmin):
     list_filter = ('date','salary','expiry')
     search_fields = ('title',)
     search_help_text = "What are you looking for ?"
+
+    fieldsets = (
+        ('Basic informations:',{
+            'fields':('title','description'),
+        }),('More informations:',{
+        'classes':('collapse',),
+            'fields':(('expiry','salary'),'slug'),
+        })
+    )
 # Register your models here.
 admin.site.register(JobPost, JobAdmin)
